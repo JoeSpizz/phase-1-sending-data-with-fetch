9e7e302e9720546e47742446407c47e69f517bb6
Joe <spizjo01@gmail.com>
2022-06-30 11:55:09 -0600

    commit tree 82ad7197ff2a4477c8ab099dc5ae13e122bc75f3

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+let user = "Steve"
+let contact = "steve@steve.com"
+function submitData(user, contact) {
+    return fetch(`http://localhost:3000/users`, config)
+    .then(res => res.json())
+    .then(obj => document.body.innerHTML = `<p> ${obj.id} </p>`)
+    .catch(function(message){
+        return document.body.innerHTML = message})
+}
+
+const config = { 
+    method: "POST",
+    headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+},
+body: JSON.stringify({
+    name: user,
+    email: contact,
+})};
+
